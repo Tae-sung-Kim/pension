c683cb977ae065a98a6d51858f31c9eff47a3ba8
김태성 <qwaszx82@danbiedu.co.kr>
2025-03-20 18:13:26 +0900

    .

--- src/pages/location/index.tsx
+++ src/pages/location/index.tsx
@@ -7,3 +7,8 @@ export default function Location() {
 
   return <div id="map" className="w-full h-96"></div>;
 }
+
+// 기상청 API로 실시간 날씨 정보
+// 한국관광공사 API로 주변 관광지 정보
+// 카카오/네이버 지도 API로 위치 및 길찾기 정보
+// ai를 활용해서 뭔가 할 수 있는게 있을까
